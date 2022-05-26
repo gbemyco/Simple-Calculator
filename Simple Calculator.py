6b35a52d0f58f40b3ee878c76df6cc5013db3062
Gbemi Abass <105248808+gbemyco@users.noreply.github.com>
2022-05-26 21:47:39 +0100

    Add files via upload

---
+++ Simple Calculator.py
@@ -0,0 +1,16 @@
+# My First Simple Calculator.
+# It performs addition(+), subtraction(-), multiplication(*) and division(/).
+a = float (input ('Enter first number= '))
+b = input ('Enter operator (+,-,*,/)= ')
+c= float (input ('Enter second number= '))
+if b == '+':
+    print (a + c)
+elif b == '-':
+    print (a - c)
+elif b == '*':
+    print (a * c)
+elif b == '/':
+    print (a / c)
+else:
+    print ('Invalid')
+#You can try this out!
